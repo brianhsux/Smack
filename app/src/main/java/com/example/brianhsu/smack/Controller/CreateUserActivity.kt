496790b4d8264001d3a1f13b47e324848fece681
Brian Hsu <brianhsux@gmail.com>
2018-03-27 00:41:55 +0800

    lesson 10

--- app/src/main/java/com/example/brianhsu/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/brianhsu/smack/Controller/CreateUserActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.brianhsu.smack.R
+import com.example.brianhsu.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -51,6 +52,10 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        AuthService.registerUser(this, "b@bh.com", "123456") { complete ->
+            if (complete) {
 
+            }
+        }
     }
 }
--- app/src/main/java/com/example/brianhsu/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/brianhsu/smack/Utilities/Constants.kt
@@ -5,5 +5,6 @@ package com.example.brianhsu.smack.Utilities
  */
 
 const val BASE_URL = "https://chattychatbh.herokuapp.com/v1/"
+//const val BASE_URL = "https://10.0.2.2:3005/v1/"
 
 const val URL_REGISTER = "${BASE_URL}account/register"
