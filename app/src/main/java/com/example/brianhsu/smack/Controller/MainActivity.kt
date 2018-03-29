0514d282bba10f2dd21314752a5d5dbde2629b3b
Brian Hsu <brianhsux@gmail.com>
2018-03-30 01:04:21 +0800

    lesson 14

--- app/src/main/java/com/example/brianhsu/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/brianhsu/smack/Controller/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.brianhsu.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.brianhsu.smack.R
+import com.example.brianhsu.smack.Services.AuthService
+import com.example.brianhsu.smack.Services.UserDataServices
+import com.example.brianhsu.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +30,24 @@ class MainActivity : AppCompatActivity() {
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
+                IntentFilter(BROADCAST_USER_DATA_CHANGE))
+    }
+
+    private val userDataChangeReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                userNameNavHeader.text = UserDataServices.name
+                userEmailNavHeader.text = UserDataServices.email
+
+                val resourceId = resources.getIdentifier(UserDataServices.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataServices.returnAvatarColor(UserDataServices.avatarColor))
+
+                loginBtnNavHeader.text = "Logout"
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -32,8 +59,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if (AuthService.isLoggedIn) {
+            // logout
+            UserDataServices.logout()
+            userNameNavHeader.text = "Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
     }
 
     fun addChannelBtnClicked(view: View) {
--- app/src/main/java/com/example/brianhsu/smack/Services/UserDataServices.kt
+++ app/src/main/java/com/example/brianhsu/smack/Services/UserDataServices.kt
@@ -1,5 +1,8 @@
 package com.example.brianhsu.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 /**
  * Created by brian on 2018/3/27.
  */
@@ -11,5 +14,36 @@ object UserDataServices {
     var email = ""
     var name = ""
 
+    fun logout() {
+        id = ""
+        avatarName = ""
+        avatarColor = ""
+        email = ""
+        name = ""
 
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
+
+    fun returnAvatarColor(components: String) : Int {
+        val strippedColor = components
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+
+        if (scanner.hasNext()) {
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(r, g, b)
+    }
 }
