833cb92422d044ae982afaf01e0425a143c9740f
Brian Hsu <brianhsux@gmail.com>
2018-04-10 23:35:36 +0800

    lesson-16

--- app/src/main/java/com/example/brianhsu/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/brianhsu/smack/Controller/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.example.brianhsu.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.example.brianhsu.smack.R
 import com.example.brianhsu.smack.Services.AuthService
@@ -24,6 +26,7 @@ class LoginActivity : AppCompatActivity() {
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
 
+        hideKeyboard()
         if (email.isNotEmpty() && password.isNotEmpty()) {
             AuthService.loginUser(this, email, password) { loginSuccess ->
                 if (loginSuccess) {
@@ -69,5 +72,13 @@ class LoginActivity : AppCompatActivity() {
         loginPasswordText.isEnabled = !enable
         loginLoginBtn.isEnabled = !enable
         loginCreateUserBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
